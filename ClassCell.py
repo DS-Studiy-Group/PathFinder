d486a8f62c47b975305bf7a1a56765c8a84322ac
Arshia-Danandeh <Arshiya.Danandeh@gmail.com>
2024-12-09 14:56:49 +0330

    Making Cell Class

--- ClassCell.py
+++ ClassCell.py
@@ -30,19 +30,3 @@ class Cell:
         if self.walls["left"]:
             pygame.draw.line(sc, pygame.Color('#060105'), (x, y + Tile), (x, y), 2)
 
-
-    def check_neighbors(self, grid):
-        neighbors = []
-        directions = [
-            (0, -1, "Up", "down"),
-            (0, 1, "down", "Up"),
-            (-1, 0, "left", "right"),
-            (1, 0, "right", "left"),
-        ]
-        for dx, dy, wall1, wall2 in directions:
-            nx, ny = self.x + dx, self.y + dy
-            if 0 <= nx < Cols and 0 <= ny < Rows:
-                neighbor = grid[ny * Cols + nx]
-                if not neighbor.visited:
-                    neighbors.append((neighbor, wall1, wall2))
-        return random.choice(neighbors) if neighbors else None
