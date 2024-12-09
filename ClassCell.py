e2bbfa56591f6f675edbb4a117ef5c0351fb63f7
Arshia-Danandeh <Arshiya.Danandeh@gmail.com>
2024-12-09 16:17:45 +0330

    Cell class in progress...

--- ClassCell.py
+++ ClassCell.py
@@ -1,10 +1,14 @@
 import pygame
-import random
 
-Tile = 20
+Tile = 50
 margin = 1
-width , height = 1000 , 600
-Rows , Cols = height // Tile , width // Tile
+Rows , Cols = 10 , 10
+width , height = 700 , Cols * Tile
+
+#colors
+WHITE = pygame.Color("white")
+BLACK = pygame.Color("black")
+GRAY = pygame.Color("gray")
 
 pygame.init()
 sc = pygame.display.set_mode((width, height))
@@ -14,19 +18,27 @@ class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.walls = {"up": True, "down": True, "left": True, "right": True}
+        self.is_wall = False
         self.visited = False
     def draw(self):
         x = self.x * Tile
         y = self.y * Tile
-        if self.visited:
-            pygame.draw.rect(sc, pygame.Color('#a9237d'), (x, y, Tile, Tile))
-        if self.walls["up"]:
-            pygame.draw.line(sc, pygame.Color('#060105'), (x, y), (x + Tile, y), 2)
-        if self.walls["right"]:
-            pygame.draw.line(sc, pygame.Color('#060105'), (x + Tile, y), (x + Tile, y + Tile), 2)
-        if self.walls["down"]:
-            pygame.draw.line(sc, pygame.Color('#060105'), (x + Tile, y + Tile), (x, y + Tile), 2)
-        if self.walls["left"]:
-            pygame.draw.line(sc, pygame.Color('#060105'), (x, y + Tile), (x, y), 2)
+        pygame.draw.rect(sc, WHITE, (x, y, Tile, Tile))
+        pygame.draw.rect(sc, GRAY, (x, y, Tile, Tile) , 2)
 
+grid = [[Cell(x, y) for x in range(Cols)] for y in range(Rows)]
+
+gameOn = True
+while gameOn:
+        sc.fill(GRAY)
+        for row in grid:
+            for cell in row:
+                cell.draw()
+    
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameOn = False
+        
+pygame.quit()
